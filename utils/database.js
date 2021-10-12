782f1bb4305bfc00d466b1fdaa0ee6dae10d7f80
jerry7657 <56625625+jerry7657@users.noreply.github.com>
2021-10-12 16:20:46 +0800

    change database setting

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,18 @@
 const { Pool } = require('pg')
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'crown_95',
-    password: '0000',
-    port: 5432,
+    connectionString: isProduction ? process.env.DATABASE_URL : `postgresql://postgres:0000@localhost:5432/crown_95`
 })
+
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'crown_95',
+//     password: '0000',
+//     port: 5432,
+// })
 
 pool.query('SELECT * from category', (err, res) => {
     console.log(JSON.stringify(res.rows));
